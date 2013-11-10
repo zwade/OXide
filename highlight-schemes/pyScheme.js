c94f234da448ed510306a3d69ed7dc8b6a8317bf
BluePichu <Matthew@Matthew-Laptop.(none)>
2013-11-10 06:07:29 -0500

    syntax support for python

---
+++ highlight-schemes/pyScheme.js
@@ -0,0 +1,94 @@
+
+                                                                     
+                                                                     
+                                                                     
+                                             
+pyScheme = {
+    "+":0,    
+    "-":0,
+    "*":0,
+    "/":0,
+    "%":0,
+    "**":0,
+    "//":0,
+    "==":0,
+    "!=":0,
+    "<>":0,
+    ">":0,
+    "<":0,
+    ">=":0,
+    "<=":0,
+    "=":0,
+    "+=":0,
+    "-=":0,
+    "*=":0,
+    "/=":0,
+    "%=":0,
+    "**=":0,
+    "//=":0,
+    "&":0,
+    "|":0,
+    "^":0,
+    "~":0,
+    "<<":0,
+    ">>":0,
+
+
+    "is":1,
+    "and":1,
+    "not":1,
+    "in":1,
+    "or":1,
+      
+    "del":2,      
+    "from":2,     
+    "while":2,
+    "as":2,       
+    "elif":2,     
+    "global":2,         
+    "with":2,
+    "assert":2,  
+    "else":2,   
+    "if":2,     
+    "pass":2,     
+    "yield":2,
+    "break":2,    
+    "except":2,   
+    "import":2,   
+    "print":2,
+    "class":2,    
+    "exec":2,      
+    "raise":2,
+    "continue":2,  
+    "finally":2,          
+    "return":2, 
+    "def":2,       
+    "for":2,       
+    "lambda":2,   
+    "try":2,
+
+    "(":3,
+    ")":3,
+    "{":3,
+    "}":3,
+    "[":3,
+    "]":3,
+    
+    "true":4,
+    "false":4,
+    
+    "sqString":5,
+    "dqString":5,
+    
+    "linCom":6
+}
+
+pyStyles = {
+    0:"font-weight:600;color:#004400;",
+    1:"font-weight:600;",
+    2:"font-weight:600;color:#0000FF;",
+    3:"font-weight:600;",
+    4:"color:#0000FF",
+    5:"color:#00FF00",
+    6:"color:#888888;text-decoration:italic;"
+}
--- highlighter.js
+++ highlighter.js
@@ -9,23 +9,51 @@ for(keyword in jsScheme){
     }
 }
 
-matchRegexStr = matchRegexStr.substr(0, matchRegexStr.length-1);
+jsReg = new RegExp(matchRegexStr, "g");
 
-genColors = function(line){
-    line = line.replace(new RegExp(matchRegexStr, "g"), function(match){
+matchRegexStr = '(#.*)|(".[^"]*"?")|(\'.[^\']*\'?)|';
+    
+for(keyword in pyScheme){
+    keyword = keyword.replace(/\+|\*|\||\^|\?/g, function(match){return "\\" + match;});
+    if(/[a-z]/.test(keyword)){
+        matchRegexStr += "(\\b" + keyword + "\\b)|";
+    } else {
+        matchRegexStr += "(" + keyword + ")|";
+    }
+}
+
+pyReg = new RegExp(matchRegexStr, "g");
+
+schema = {
+    "js":jsReg,
+    "py":pyReg
+};
+
+styles = {
+    "js":jsStyles,
+    "py":pyStyles
+};
+
+commentDelimiters = {
+    "js":"//",
+    "py":"#"
+}
+
+genColors = function(line, lang){
+    line = line.replace(jsReg, function(match){
         if(match == ""){
             return "";
         }
         if(match.charAt(0) == "'"){
-            return '<span style="' + jsStyles[jsScheme["sqString"]] + '">' + match + '</span>';
+            return '<span style="' + styles[lang][schema[lang]["sqString"]] + '">' + match + '</span>';
         }
         if(match.charAt(0) == '"'){
-            return '<span style="' + jsStyles[jsScheme["dqString"]] + '">' + match + '</span>';
+            return '<span style="' + styles[lang][schema[lang]["dqString"]] + '">' + match + '</span>';
         }
-        if(match.charAt(0) == "/"){
-            return '<span style="' + jsStyles[jsScheme["linCom"]] + '">' + match + '</span>';
+        if(match.charAt(0) == commentDelimiters[lang]){
+            return '<span style="' + styles[lang][schema[lang]["linCom"]] + '">' + match + '</span>';
         }
-        return '<span style="' + jsStyles[jsScheme[match]] + '">' + match + '</span>';
+        return '<span style="' + styles[lang][schema[lang][match]] + '">' + match + '</span>';
     });
     
     return line;
