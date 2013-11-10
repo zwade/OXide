336af9e773105ea9ab726f3fe7aa728d1298780a
Zach Wade <zacharywade@gmail.com>
2013-11-10 02:19:37 -0500

    smarter syntax

--- highlighter.js
+++ highlighter.js
@@ -1,19 +1,19 @@
 genColors = function(line){
-<<<<<<< HEAD
     //comments
-    line.replace(new RegExp("//.*", "g"), function(match){return '<span style="' + jsStyles[jsScheme["linCom"]] + '">' + match + '</span>';});
+    line = line.replace(new RegExp("//.*", "g"), function(match){return '<span style="' + jsStyles[jsScheme["linCom"]] + '">' + match + '</span>';});
     
     //strings
-    line.replace(new RegExp('".[^"]*"?"', "g"), function(match){return '<span style="' + jsStyles[jsScheme["dqString"]] + '">' + match + '</span>';});
+    line = line.replace(new RegExp('".[^"]*"?"', "g"), function(match){return '<span style="' + jsStyles[jsScheme["dqString"]] + '">' + match + '</span>';});
     
-    line.replace(new RegExp("'.[^']*'?", "g"), function(match){return '<span style="' + jsStyles[jsScheme["sqString"]] + '">' + match + '</span>';});
+    line = line.replace(new RegExp("'.[^']*'?", "g"), function(match){return '<span style="' + jsStyles[jsScheme["sqString"]] + '">' + match + '</span>';});
     
     //all the rest
     for(keyword in jsScheme){
-        line.replace(keyword, '<span style="' + jsStyles[jsScheme[keyword]] + '">' + keyword + '</span>');
+        line = line.replace(keyword, '<span style="' + jsStyles[jsScheme[keyword]] + '">' + keyword + '</span>');
     }
+    return line
 }
-=======
+/**
     out = "";
     var tmp = line.split(" ");
     for(word in tmp){
@@ -25,5 +25,4 @@ genColors = function(line){
         out += " ";
     }
     return out.substr(0, out.length-1);
-}
->>>>>>> 755f528ba3d6303a67ce3923c3fabf820efa8632
+}**/
